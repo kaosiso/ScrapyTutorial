ef161828491052411c101a72bbdc153d70980793
Kaosiso <kaosisochukwum@gmail.com>
2025-07-12 21:54:05 +0800

    Build Discovery & Extraction Spider & Cleaning Data with Item Pipelines

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,70 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        # 1. Strip all whitespaces from strings (except 'description')
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                if isinstance(value, str):
+                    adapter[field_name] = value.strip()
+
+        # 2. Convert 'category' and 'product_type' to lowercase
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            if isinstance(value, str):
+                adapter[lowercase_key] = value.lower()
+
+        # 3. Convert price-related fields to float
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            if isinstance(value, str):
+                value = value.replace('€', "").strip()
+                try:
+                    adapter[price_key] = float(value)
+                except ValueError:
+                    adapter[price_key] = 0.0
+
+        # 4. Extract number of books in stock from 'availability'
+        availability_string = adapter.get('availability', '')
+        if isinstance(availability_string, str):
+            split_string_array = availability_string.split("(")
+            if len(split_string_array) < 2:
+                adapter["availability"] = 0
+            else:
+                availability_array = split_string_array[1].split(' ')
+                try:
+                    adapter["availability"] = int(availability_array[0])
+                except (IndexError, ValueError):
+                    adapter["availability"] = 0
+
+        # 5. Convert 'num_reviews' from string to int
+        num_reviews_string = adapter.get('num_reviews')
+        try:
+            adapter['num_reviews'] = int(num_reviews_string)
+        except (TypeError, ValueError):
+            adapter['num_reviews'] = 0
+
+        # 6. Convert star ratings from text to integer
+        stars_string = adapter.get('stars', '')
+        if isinstance(stars_string, str):
+            split_stars_array = stars_string.split(' ')
+            if len(split_stars_array) > 1:
+                stars_text_value = split_stars_array[1].lower()
+                stars_dict = {
+                    "zero": 0,
+                    "one": 1,
+                    "two": 2,
+                    "three": 3,
+                    "four": 4,
+                    "five": 5
+                }
+                adapter['stars'] = stars_dict.get(stars_text_value, 0)
+            else:
+                adapter['stars'] = 0
+
         return item
--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import BookItem  # Make sure this is the correct import
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
@@ -9,11 +10,12 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css('article.product_pod')
 
         for book in books:
-            yield {
-                'name': book.css('h3 a::attr(title)').get(),
-                'price': book.css('.product_price .price_color::text').get(),
-                'url': book.css('h3 a::attr(href)').get(),
-            }
+            relative_url = book.css('h3 a::attr(href)').get()
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+            yield response.follow(book_url, callback=self.parse_book_page)
 
         next_page = response.css('li.next a::attr(href)').get()
         if next_page:
@@ -23,3 +25,21 @@ class BookspiderSpider(scrapy.Spider):
                 next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
             yield response.follow(next_page_url, callback=self.parse)
 
+    def parse_book_page(self, response):
+        table_rows = response.css("table tr")
+        book_item = BookItem()
+
+        book_item['url'] = response.url
+        book_item['title'] = response.css('.product_main h1::text').get()
+        book_item['product_type'] = table_rows[1].css('td::text').get()
+        book_item['price_excl_tax'] = table_rows[2].css('td::text').get()
+        book_item['price_incl_tax'] = table_rows[3].css('td::text').get()
+        book_item['tax'] = table_rows[4].css('td::text').get()
+        book_item['availability'] = table_rows[5].css('td::text').get()
+        book_item['num_reviews'] = table_rows[6].css('td::text').get()
+        book_item['stars'] = response.css("p.star-rating").attrib['class']
+        book_item['category'] = response.xpath('//ul[@class="breadcrumb"]/li[position()=last()-1]/a/text()').get()
+        book_item['description'] = response.xpath('//div[@id="product_description"]/following-sibling::p[1]/text()').get()
+        book_item['price'] = response.xpath('//p[@class="price_color"]/text()').get()
+
+        yield book_item
